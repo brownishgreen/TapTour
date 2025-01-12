8d5c59239d9e25db81e1fa5ecd454f7ba17c952f
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-01-12 19:37:35 +0800

    fix: resolve merge conflicts in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/Header'
 import RegisterForm from './components/RegisterForm'
+import Header from './components/Header'
 
 function App() {
   return (
