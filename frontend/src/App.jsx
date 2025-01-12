a679b4872fb75d358eb7f8d5767453b5d990de2b
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-01-12 20:07:12 +0800

    feat(client): add Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import RegisterForm from './components/RegisterForm'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <RegisterForm />
+      <Footer />
     </div>
   )
 }
