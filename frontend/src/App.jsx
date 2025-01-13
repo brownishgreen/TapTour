8160bfe4f9bf5ea46206010678abf77147fe448f
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-01-13 10:32:29 +0800

    refactor: use SCSS variables for colors and remove unnecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RegisterForm from './components/RegisterForm'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -6,9 +7,9 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="app">
-      <Header />
-      <RegisterForm />
-      <Footer />
+      <Header /> 
+      <RegisterForm /> 
+      <Footer /> 
     </div>
   )
 }
