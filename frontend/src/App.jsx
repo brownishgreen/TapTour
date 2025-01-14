07fc396a3f9bc91598e77bbd0ca3171f829ac003
Ryan <youcen666@gamil.com>
2025-01-15 03:14:46 +0800

    feat: add user dropdown menu after login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>首頁</h1>} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
