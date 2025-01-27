b62ad4ac4ebf6d33eccb3ab7411d1a17db1b695e
Ryan <youcen666@gamil.com>
2025-01-27 22:11:56 +0800

    add follow user functionality to followerController

---
+++ backend/migrations/20250127071951-create-followers.js
@@ -0,0 +1,47 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Followers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      follower_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // 關聯到 Users 表
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      following_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // 關聯到 Users 表
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Followers')
+  },
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router()
 const userRoutes = require('./user')
 const adminRoutes = require('./admin')
 const activityRoutes = require('./activity')
+const followerRoutes = require('./follower')
+
 // 掛載路由模組
 router.use('/users', userRoutes) // 路徑 /api/users
 router.use('/admin', adminRoutes) // 路徑 /api/admin
 router.use('/activities', activityRoutes) // 路徑 /api/activities
+router.use('/followers', followerRoutes) // 路徑 /api/admin
+
 
 module.exports = router
