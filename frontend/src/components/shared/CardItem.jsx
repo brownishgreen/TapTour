8efc9bc1c16bbfde5aadeaddaea1e70229f6ac5f
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-01-30 00:25:49 +0800

    feat: Implement activity listing and detail with improved UI and image rendering

--- frontend/src/components/shared/CardItem.jsx
+++ frontend/src/components/shared/CardItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 
-const CardItem = ({ buttonText, image, title, subtitle, description }) => {
+const CardItem = ({ buttonText, image, title, subtitle,activityLink}) => {
   return (
     <Card className="card-item">
       <Card.Img variant="top" src={image} />
       <Card.Body>
+        <Card.Subtitle className="mb-2 text-muted card-item__button-subtitle">{subtitle}</Card.Subtitle>
         <Card.Title>{title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
-        <Card.Text>{description}</Card.Text>
-        <Button variant="secondary">{buttonText}</Button>
       </Card.Body>
+      <div className="card-item__button">
+        <Button variant="secondary" href={activityLink}>{buttonText}</Button>
+      </div>
     </Card>
   )
 }
