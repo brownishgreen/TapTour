3cbd14eb5b5206920abc1618ddf24c5fbf312550
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-02-05 23:22:52 +0800

    refactor(activity-detail-page):render time-duration

--- frontend/src/components/activity/ActivityDetailIntroduction.jsx
+++ frontend/src/components/activity/ActivityDetailIntroduction.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPeopleGroup, faClock, faCheck, faEarthEurope } from '@fortawesome/free-solid-svg-icons'
 
-const ActivityDetailIntroduction = () => {
+const ActivityDetailIntroduction = ({ timeDuration }) => {
   return (
     <div className="activity-detail-introduction">
       <p><FontAwesomeIcon icon={faPeopleGroup} /> 0-99歲</p>
-      <p><FontAwesomeIcon icon={faClock} /> 所需時間：5-7小時</p>
+      <p><FontAwesomeIcon icon={faClock} /> 所需時間：{timeDuration}小時</p>
       <p><FontAwesomeIcon icon={faClock} /> 開始時間：查看供應狀況</p>
       <p><FontAwesomeIcon icon={faEarthEurope} /> 即時指南：中文、英語</p>
       <p><FontAwesomeIcon icon={faCheck} /> 包含：午餐、交通、門票</p>
