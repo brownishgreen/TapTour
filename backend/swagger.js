1fefd81cc91b8653990b3673c10391c1e9da0f90
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-02-09 02:32:37 +0800

    feat: API documentation tag orders

--- backend/swagger.js
+++ backend/swagger.js
@@ -14,7 +14,45 @@ const options = {
                 '與旅遊活動、商品、景點、留言相關的操作。\n\n' +
                 '整合了驗證系統與安全機制，確保資料傳輸的安全性，\n' +
                 '並以 RESTful 標準設計，方便開發者串接。'
-        }
+        },
+        tags: [
+            {
+                name: 'Admin',
+                description: '管理員相關操作'
+            },
+            {
+                name: 'Users',
+                description: '用戶相關操作'
+            },
+            {
+                name: 'Followers',
+                description: '追蹤者相關操作'
+            },
+            {
+                name: 'Orders',
+                description: '訂單相關操作'
+            },
+            {
+                name: 'Activities',
+                description: '活動相關操作'
+            },
+            {
+                name: 'Categories',
+                description: '活動類別相關操作'
+            },
+            {
+                name: 'Products',
+                description: '商品相關操作'
+            },
+            {
+                name: 'Locations',
+                description: '地點相關操作'
+            },
+            {
+                name: 'Comments',
+                description: '評論相關操作'
+            }
+        ]
     },
     apis: ['./routes/*.js'] //指定包含 Swagger 註解的文件所在的檔案位置
 }
