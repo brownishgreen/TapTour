c9c49792329855bd6ad87b5ef3971d1f49460fac
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-02-14 15:47:30 +0800

    feat: Implement favorite migration and model

---
+++ backend/migrations/20250214073149-create-favorites.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('favorites', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', 
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      itemId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      itemType: {
+        type: Sequelize.ENUM('activity', 'product'),
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('favorites');
+  }
+};
