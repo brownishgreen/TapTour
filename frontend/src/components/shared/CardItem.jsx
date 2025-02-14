14caea790ae09dc5d8f3d2475194e77669857699
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-02-14 15:25:36 +0800

    implement heart button in CardItem component

--- frontend/src/components/shared/CardItem.jsx
+++ frontend/src/components/shared/CardItem.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const   CardItem = ({ buttonText, image, title, subtitle, cardLink }) => {
   const location = useLocation()
   const isHomepage = location.pathname === '/'
+
   return (
     <Card className={`card-item ${isHomepage ? 'homepage-card-item' : ''}`}>
       <Card.Img variant="top" src={image} alt={title} className="card-item__top" />
@@ -14,6 +17,9 @@ const   CardItem = ({ buttonText, image, title, subtitle, cardLink }) => {
         <Card.Title>{title}</Card.Title>
       </Card.Body>
       <div className="card-item__button">
+        <button className="favorite-button">
+          <FontAwesomeIcon icon={faHeart} />
+        </button>
         <Link to={cardLink}>
           <Button variant="secondary">{buttonText}</Button>
         </Link>
