f7b9f967b2b703b2a6d75750fc4e682f6aa67ca2
Ryan <youcen666@gamil.com>
2025-02-16 14:52:40 +0800

    Update API connection to use the official backend URL

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://35.201.179.41:8080/api',
+  baseURL: 'https://taptour-backend.yuanologue.com',
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true
+  withCredentials: true,
 })
 
 export default apiClient
