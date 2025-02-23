2a0669f6ca2d8e5715452d73eba6099d79648d4b
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-02-23 22:27:44 +0800

    update: CORS setting

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,8 @@ app.use(
         callback(new Error('Not allowed by CORS'));
       }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
   })
 )
