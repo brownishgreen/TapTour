87de451c3832e46f51ad2aa32c3db56d2c724fe6
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-03-05 00:34:45 +0800

    Fix: google oauth api and passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -9,13 +9,20 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
   try {
-    const user = {
-      id: profile.id,
-      email: profile.emails[0].value,
-      name: profile.displayName,
-      avatar: profile.photos[0].value
+    let user = await User.findOne({
+      where: {
+        email: profile.emails[0].value
+      }
+    })
+    if (!user) {
+      user = await User.create({
+        id: profile.id,
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        avatar: profile.photos[0].value
+      })
     }
     done(null, user)
   } catch (error) {
